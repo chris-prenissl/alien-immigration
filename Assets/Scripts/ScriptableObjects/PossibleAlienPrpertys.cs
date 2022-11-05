a0bf665f2c291358d374a66a4344d1f623e9715a
Unnamed_DEV <a22yo22io@gmail.com>
2022-11-05 12:35:21 +0100

    Added Alien propertys can or can't pass

--- Assets/Scripts/ScriptableObjects/PossibleAlienPrpertys.cs
+++ Assets/Scripts/ScriptableObjects/PossibleAlienPrpertys.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PossibleAlienPrpertys")]
 public class PossibleAlienPrpertys : ScriptableObject
 {
+    [Header("Picture")]
     public List<Sprite> picture;
 
+    [Header("Names")]
     public List<string> names;
+
+    [Header("Occupation")]
     public List<string> occupation;
+    public List<string> canPassOccupation;
+    public List<string> canNotPassOccupation;
+
+    [Header("HomePlanet")]
     public List<string> homePlanet;
+    public List<string> canPassPlanet;
+    public List<string> canNotPlanet;
+
+    [Header("Species")]
     public List<string> species;
+
+    [Header("Age")]
     public int age;
 }
