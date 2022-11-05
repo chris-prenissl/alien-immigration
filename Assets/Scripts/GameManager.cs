22f1a4fa8c1ac4125b842c82858a9287ead651ab
EndocrineBAMSc <bamsc.studio@gmail.com>
2022-11-05 09:13:50 +0100

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+
+namespace AlienImmigration
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum GameState
     {
-        
+        MainMenu,
+        Starting,
+        NewAlien,
+        BoothTime,
+        DecisionCheck,
+        Highscore,
+        Ending
     }
 
-    // Update is called once per frame
-    void Update()
+    public class GameManager : MonoBehaviour
     {
-        
+        #region Fields
+        public static GameManager Instance;
+
+
+        #endregion
+
+        #region Properties
+        private GameState gameState;
+
+        public GameState GameState
+        {
+            get { return gameState; }
+            private set { gameState = value; }
+        }
+
+        #endregion
+
+        #region Public Functions
+
+        public void SwitchState(GameState state)
+        {
+            switch (state)
+            {
+                case (GameState.MainMenu):
+                    break;
+
+                case (GameState.Starting):
+                    break;
+
+                case (GameState.NewAlien):
+                    break;
+
+                case (GameState.BoothTime):
+                    break;
+
+                case (GameState.Highscore):
+                    break;
+
+                case (GameState.Ending):
+                    break;        
+            }
+        }
+
+
+        #endregion 
+
+
     }
 }
